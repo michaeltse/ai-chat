31f8970534d40639d15f9463bc41d7b0e937f4b8
michaeltse <pakho.tse@gmail.com>
2025-02-11 21:45:51 +0800

    resolve the bug

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("var GROQ_API_KEY = \"your_actual_api_key_here\"; // This will be replaced during the build\n\n// Your existing code... \n\nfunction generateResponse() {\n  var input = document.getElementById('promptInput');\n  var userMessage = input.value.trim();\n  if (!userMessage) {\n    alert('Please enter a message');\n    return;\n  }\n\n  // Logic to handle the user message\n  console.log(\"User message:\", userMessage);\n  // Add your existing code for sending the message to the API\n}\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
